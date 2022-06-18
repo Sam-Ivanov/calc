06c37d05946ab1fb4cc1065e146b844331f0c9b8
Aleksandr Ivanov <1300087@gmail.com>
2022-06-18 07:12:30 +0300

    add '00'

--- js/calculator.js
+++ js/calculator.js
@@ -3,7 +3,7 @@ let b = '';
 let sign = '';
 let finish = false;
 
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '00', '.'];
 const action = ['-', '+', 'X', '÷'];
 
 const out = document.querySelector('.calc__out');
